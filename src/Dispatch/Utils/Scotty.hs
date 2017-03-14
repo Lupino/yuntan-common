ec93645b6e88bb270241ef3f186caad399a6ae7d
Lupino <lmjubuntu@gmail.com>
2017-03-14 21:59:16 +0800

    Add scotty helper

--- src/Dispatch/Utils/Scotty.hs
+++ src/Dispatch/Utils/Scotty.hs
@@ -5,23 +5,45 @@ module Dispatch.Utils.Scotty
     maybeNotFound
   , eitherNotFound
   , eitherBadRequest
+  , err
+  , ok
+  , errNotFound
+  , errBadRequest
+  , okListResult
   ) where
 
-import           Dispatch.Types.Result (ErrResult, err)
-import           Network.HTTP.Types    (status400, status404)
-import           Web.Scotty.Trans      (ActionT, ScottyError, json, status)
+import           Dispatch.Types.ListResult (ListResult, fromListResult)
+import qualified Dispatch.Types.Result     as R (ErrResult, err, fromOkResult,
+                                                 ok)
+import           Network.HTTP.Types        (Status, status400, status404)
+import           Web.Scotty.Trans          (ActionT, ScottyError, json, status)
 
-import           Data.Aeson            (ToJSON)
+import           Data.Aeson                (ToJSON)
+import           Data.Text                 (Text)
 
 maybeNotFound :: (ToJSON a, ScottyError e, Monad m) => String -> Maybe a -> ActionT e m ()
-maybeNotFound _ (Just a) = json a
-maybeNotFound obj Nothing = status status404
-                            >> json (err $ obj ++ " not found.")
+maybeNotFound _ (Just a)  = json a
+maybeNotFound obj Nothing = err status404 $ obj ++ " not found."
 
-eitherBadRequest :: (ToJSON a, ScottyError e, Monad m) => Either ErrResult a -> ActionT e m ()
+eitherBadRequest :: (ToJSON a, ScottyError e, Monad m) => Either R.ErrResult a -> ActionT e m ()
 eitherBadRequest (Right a) = json a
 eitherBadRequest (Left e)  = status status400 >> json e
 
-eitherNotFound :: (ToJSON a, ScottyError e, Monad m) => Either ErrResult a -> ActionT e m ()
+eitherNotFound :: (ToJSON a, ScottyError e, Monad m) => Either R.ErrResult a -> ActionT e m ()
 eitherNotFound (Right a) = json a
 eitherNotFound (Left e)  = status status404 >> json e
+
+err :: (ScottyError e, Monad m) => Status -> String -> ActionT e m ()
+err st msg = status st >> json (R.err msg)
+
+ok :: (ToJSON a, ScottyError e, Monad m) => Text -> a -> ActionT e m ()
+ok key = json . R.fromOkResult key . R.ok
+
+errNotFound :: (ScottyError e, Monad m) => String -> ActionT e m ()
+errNotFound = err status404
+
+errBadRequest :: (ScottyError e, Monad m) => String -> ActionT e m ()
+errBadRequest = err status400
+
+okListResult :: (ToJSON a, ScottyError e, Monad m) => Text -> ListResult a -> ActionT e m ()
+okListResult key = json . fromListResult key

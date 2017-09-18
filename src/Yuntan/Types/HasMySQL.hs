4b5546255a30da6858ee097bc1b5ec6e7c20a2f0
Lupino <lmjubuntu@gmail.com>
2017-09-18 16:30:16 +0800

    add simpleEnv

--- src/Yuntan/Types/HasMySQL.hs
+++ src/Yuntan/Types/HasMySQL.hs
@@ -4,6 +4,8 @@ module Yuntan.Types.HasMySQL
   , HasMySQL
   , mysqlPool
   , tablePrefix
+  , SimpleEnv
+  , simpleEnv
   ) where
 
 
@@ -15,3 +17,14 @@ type TablePrefix = String
 class HasMySQL u where
   mysqlPool :: u -> Pool Connection
   tablePrefix :: u -> TablePrefix
+
+data SimpleEnv = SimpleEnv { pc :: Pool Connection
+                           , pf :: String
+                           }
+
+instance HasMySQL SimpleEnv where
+  mysqlPool = pc
+  tablePrefix = pf
+
+simpleEnv :: Pool Connection -> TablePrefix -> SimpleEnv
+simpleEnv pool prefix = SimpleEnv{pc=pool, pf = prefix}

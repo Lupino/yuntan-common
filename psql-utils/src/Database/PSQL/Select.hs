3ca2761a7653b81a38a8669aedcbe790219391ec
Lupino <lmjubuntu@gmail.com>
2026-03-23 11:19:10 +0800

    psql-utils: validate placeholder counts in select queries

--- psql-utils/src/Database/PSQL/Select.hs
+++ psql-utils/src/Database/PSQL/Select.hs
@@ -36,7 +36,8 @@ module Database.PSQL.Select
 
 
 import           Data.Int                           (Int64)
-import           Data.Char                          (isSpace)
+import           Data.Char                          (isAlphaNum, isLetter, isSpace)
+import           Data.List                          (isPrefixOf)
 import           Data.Maybe                         (listToMaybe)
 import           Database.PostgreSQL.Simple         (Only (..))
 import           Database.PostgreSQL.Simple.FromRow (FromRow (..))
@@ -51,11 +52,17 @@ import           Database.PSQL.Types.TableName      (TableName)
 import           Database.PSQL.Util                 (getOnlyDefault)
 
 selectRaw :: (ToRow a, FromRow b) => TableName -> Columns -> String -> a -> Page -> GroupBy -> PSQL [b]
-selectRaw tn cols partSql a p g = query sql a
+selectRaw tn cols partSql a p g
+  | expectedCount == length rowParams = query sql a
+  | otherwise = error "selectRaw: placeholder count does not match params"
   where sql = genSelect tn cols partSql p g
+        rowParams = toRow a
+        expectedCount = countPlaceholders partSql
 
 selectRaw_ :: FromRow b => TableName -> Columns -> String -> Page -> GroupBy -> PSQL [b]
-selectRaw_ tn cols partSql p g = query_ sql
+selectRaw_ tn cols partSql p g
+  | countPlaceholders partSql == 0 = query_ sql
+  | otherwise = error "selectRaw_: placeholders present but no params provided"
   where sql = genSelect tn cols partSql p g
 
 selectAllRaw :: (ToRow a, FromRow b) => TableName -> Columns -> String -> a -> GroupBy -> PSQL [b]
@@ -65,12 +72,92 @@ selectAllRaw_ :: FromRow b => TableName -> Columns -> String -> GroupBy -> PSQL 
 selectAllRaw_ tn cols partSql = selectRaw_ tn cols partSql pageNone
 
 selectInRaw :: (ToField a, ToRow r, FromRow b) => TableName -> Columns -> Column -> [a] -> String -> r -> Page -> GroupBy -> PSQL [b]
-selectInRaw tn cols col xs partSql r = selectRaw tn cols ids ys
+selectInRaw tn cols col xs partSql r
+  | expectedCount == length ys = selectRaw tn cols ids ys
+  | otherwise = error "selectInRaw: placeholder count does not match params"
   where (inSql, ixs) = genIn col xs
         ids = inSql `genAnd` partSql
+        rowParams = toRow r
         ys
-          | all isSpace partSql = ixs
-          | otherwise = ixs ++ toRow r
+          | all isSpace partSql =
+            case rowParams of
+              [] -> ixs
+              _  -> error "selectInRaw: non-empty params with blank partSql"
+          | otherwise = ixs ++ rowParams
+        expectedCount = countPlaceholders ids
+
+countPlaceholders :: String -> Int
+countPlaceholders = goNormal False 0
+  where
+    goNormal :: Bool -> Int -> String -> Int
+    goNormal _ n []                = n
+    goNormal _ n ('?':'?':rest)    = goNormal False n rest
+    goNormal _ n ('?':rest)        = goNormal False (n + 1) rest
+    goNormal _ n ('\'':rest)       = goSingleQuoted n rest
+    goNormal _ n ('"':rest)        = goDoubleQuoted n rest
+    goNormal prevIsIdent n ('$':rest)
+      | not prevIsIdent =
+          case parseDollarTag rest of
+            Just (tag, body) -> goDollarQuoted n tag body
+            Nothing          -> goNormal True n rest
+      | otherwise = goNormal True n rest
+    goNormal _ n ('-':'-':rest)    = goLineComment n rest
+    goNormal _ n ('/':'*':rest)    = goBlockComment n 1 rest
+    goNormal _ n (c:rest)          = goNormal (isIdentChar c) n rest
+
+    goSingleQuoted :: Int -> String -> Int
+    goSingleQuoted n []                = n
+    goSingleQuoted n ('\'':'\'':rest)  = goSingleQuoted n rest
+    goSingleQuoted n ('\'':rest)       = goNormal False n rest
+    goSingleQuoted n (_:rest)          = goSingleQuoted n rest
+
+    goDoubleQuoted :: Int -> String -> Int
+    goDoubleQuoted n []              = n
+    goDoubleQuoted n ('"':'"':rest)  = goDoubleQuoted n rest
+    goDoubleQuoted n ('"':rest)      = goNormal False n rest
+    goDoubleQuoted n (_:rest)        = goDoubleQuoted n rest
+
+    goLineComment :: Int -> String -> Int
+    goLineComment n []           = n
+    goLineComment n ('\n':rest)  = goNormal False n rest
+    goLineComment n (_:rest)     = goLineComment n rest
+
+    goBlockComment :: Int -> Int -> String -> Int
+    goBlockComment n _ []                    = n
+    goBlockComment n d ('/':'*':rest)        = goBlockComment n (d + 1) rest
+    goBlockComment n 1 ('*':'/':rest)        = goNormal False n rest
+    goBlockComment n d ('*':'/':rest)        = goBlockComment n (d - 1) rest
+    goBlockComment n d (_:rest)              = goBlockComment n d rest
+
+    goDollarQuoted :: Int -> String -> String -> Int
+    goDollarQuoted n tag = go
+      where delimiter = '$' : tag ++ "$"
+            delimiterLen = length delimiter
+
+            go [] = n
+            go xs
+              | delimiter `isPrefixOf` xs = goNormal False n (drop delimiterLen xs)
+              | otherwise =
+                  case xs of
+                    _:rest -> go rest
+
+    parseDollarTag :: String -> Maybe (String, String)
+    parseDollarTag xs =
+      case break (== '$') xs of
+        (tag, '$':rest)
+          | isValidDollarTag tag -> Just (tag, rest)
+        _ -> Nothing
+
+    isValidDollarTag :: String -> Bool
+    isValidDollarTag "" = True
+    isValidDollarTag (c:cs) =
+      (isLetter c || c == '_') && all isDollarTagChar cs
+
+    isDollarTagChar :: Char -> Bool
+    isDollarTagChar c = isAlphaNum c || c == '_'
+
+    isIdentChar :: Char -> Bool
+    isIdentChar c = isAlphaNum c || c == '_' || c == '$'
 
 select :: (ToRow a, FromRow b) => TableName -> Columns -> String -> a -> Page -> PSQL [b]
 select tn cols partSql a p = selectRaw tn cols partSql a p groupNone
